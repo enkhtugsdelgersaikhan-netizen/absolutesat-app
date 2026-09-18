b328e5540a5c7414c329d4651e6e55d57cc8d30b
enkhtugsdelgersaikhan-netizen <enkhtugsdelgersaikhan@gmail.com>
2026-09-18 10:22:56 +0800

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ikvvixdyztyqqxkveois.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_nO5HUWPidf4U_MMK0-HYEA_vuOR0POg";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
